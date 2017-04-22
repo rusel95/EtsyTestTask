8d686ba85ed7094afb999ff001750a9d7a2305ef
Admin <ruslanpopesku95@gmail.com>
2017-04-22 15:49:33 +0300

    Add tab bar

--- EtsyTestTask/SearchViewController.swift
+++ EtsyTestTask/SearchViewController.swift
@@ -8,12 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        EtsyAPI.shared.getProducts(inCategory: "Art")
+    
         
     }
 
